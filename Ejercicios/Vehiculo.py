037e743cf57e11680ff844a1889368ed30c5feb4
belac <belacaste3@gmail.com>
2023-02-15 09:47:44 -0500

    Initial Commit

---
+++ Ejercicios/Vehiculo.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+
+    class Vehiculo:
+        def __init__(self, velocidad_maxima, kilometraje):
+            self.velocidad_maxima= velocidad_maxima
+            self.kilometraje= kilometraje
---
+++ README.md
@@ -0,0 +1,3 @@
+# Taller 3: CONCEPTOS POO
+
+Este es un taller de participación con algunos ejercicios donde se aplican los conceptos de programación orientada a objetos
