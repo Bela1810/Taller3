deb99002ebd9c0d951f0060e2936fa3a52ef2a11
belac <belacaste3@gmail.com>
2023-02-15 21:20:07 -0500

    Initial Commit

---
+++ Ejercicios/Carta.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+
+    PICA= ("As", "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez", "J", "Q", "K")
+    CORAZON= ("As", "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez", "J", "Q", "K")
+    DIAMANTE= ("As", "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez", "J", "Q", "K")
+    TREBOL= ("As", "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez", "J", "Q", "K")
+
+    class Carta:
+        def __init__(self, valor: str, pinta: str):
+            self.valor = valor
+            self.pinta = pinta
+
+
+
+
