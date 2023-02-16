4dffd3b0fd833426d48c5927cefa7318868d34c3
belac <belacaste3@gmail.com>
2023-02-15 21:13:04 -0500

    Initial Commit

--- Ejercicios/Circulo.py
+++ Ejercicios/Circulo.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
 
     import math
 
-    pi = math.pi
+    PI = math.pi
 
     class Circulo:
         def __init__(self, centro, radio):
@@ -14,14 +14,14 @@ if __name__ == "__main__":
             print(f"El area del circulo es: {area}")
 
         def perimetro_circulo(self):
-            perimetro= 2 * pi * self.radio
+            perimetro= 2 * PI * self.radio
             print(f"El perimetro del circulo es: {perimetro}")
 
         def pertenece_circulo(self):
             pass
 
-circulo_1 = Circulo( 4, 6 )
-circulo_1.area_circulo()
-circulo_1.perimetro_circulo()
+    circulo_1 = Circulo( 4, 6 )
+    circulo_1.area_circulo()
+    circulo_1.perimetro_circulo()
 
 # si un punto pertenece al círculo o no.
