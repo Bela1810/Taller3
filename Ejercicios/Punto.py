0023d13aa4510b35f91d0c5c17547336f472aec3
belac <belacaste3@gmail.com>
2023-02-15 21:08:36 -0500

    Initial Commit

--- Ejercicios/Punto.py
+++ Ejercicios/Punto.py
@@ -3,27 +3,36 @@ if __name__ == "__main__":
     import math
 
     class Punto:
-        def __init__(self, x, y):
+        def __init__(self, x: int, y: int):
             self.x = x
             self.y = y
 
         def mostrar(self):
             print(f"El punto en x es: {self.x}")
             print(f"El punto en y es: {self.y}")
-            print((f"Coordenadas ({self.x},{self.y}"))
+            print((f"Sus coordenadas ({self.x},{self.y})"))
 
         def mover(self, mover_x, mover_y):
             self.x += mover_x
             self.y += mover_y
+            print(f"({mover_x},{mover_y})")
 
-        def calcular_distancia(self):
-            pass
+        def calcular_distancia(self, otro_punto_x, otro_punto_y):
+            distancia_x = self.x + otro_punto_x
+            distancia_y = self.y + otro_punto_y
+            print(math.sqrt((distancia_x**2+distancia_y**2)))
+
+    punto_1 = Punto(3, 6)
+    punto_1.mostrar()
+    punto_1.mover(2, 4)
+    punto_1.calcular_distancia(4, 2)
 
 
 
-##A la clase del punto anterior, defínale los siguientes métodos:
 
-#- Un método calcular_distancia que calcule la distancia de la instancia actual con otro punto.
+
+
+
 
 
 
