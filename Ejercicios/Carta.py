fa7b01daae2a29c42e3367383841e6e1bdb46f7b
Bela1810 <110506001+Bela1810@users.noreply.github.com>
2023-02-16 12:18:47 -0500

    Update Carta.py

--- Ejercicios/Carta.py
+++ Ejercicios/Carta.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
 
-    PICA= ("Pica")
-    CORAZON= ("Corazon")
-    DIAMANTE= ("Diamante")
+    PICA= "Pica"
+    CORAZON= "Corazon"
+    DIAMANTE= "Diamante"
     TREBOL= "Trebol"
 
     class Carta:
