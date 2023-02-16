fcac62c3e354a53fae5de07728d0d9a02dfa3e29
belac <belacaste3@gmail.com>
2023-02-15 20:54:13 -0500

    Initial Commit

--- Ejercicios/Vehiculo.py
+++ Ejercicios/Vehiculo.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
 
     class Vehiculo:
-        def __init__(self, velocidad_maxima, kilometraje):
+        def __init__(self, velocidad_maxima: float, kilometraje: float):
             self.velocidad_maxima= velocidad_maxima
             self.kilometraje= kilometraje
